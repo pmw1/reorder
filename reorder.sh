1831a3ba108b625a605eae122ca147d7e3744284
root <root@andersen.burlingtontelecom.net>
2017-07-18 20:52:33 -0400

    v1

--- reorder.sh
+++ reorder.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo docker kill stream-split
-sudo docker rm -f stream-split
+sudo docker rm -f reorder
 echo Inter-Docker user: $USER
-sudo docker run -v $HOME/apps/reorder/hostfiles/:/hostfiles -p 3000:3000/udp -p 3005:3005/tcp --name="reorder" --network="split" --ip="10.0.10.4" --privileged -i -t -d pmw1/reorder
+sudo docker run -v $HOME/apps/reorder/hostfiles/:/hostfiles -p 3000:3000/udp -p 3005:3005/tcp --name="reorder" --network="split" --ip="10.0.10.4" --privileged -i -t  pmw1/reorder
